dca8eca9eab76bd3141e380904f9cce278dd1afe
AdrianClv <adrianclv@gmail.com>
2017-06-05 23:15:30 +0200

    new version

--- bot.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by hapax on 26/05/17.
- */
--- migrations/2_deploy_betfunding.js
+++
@@ -1,6 +0,0 @@
-var Betfunding = artifacts.require("Betfunding");
-
-module.exports = function(deployer) {
-  // Pass 42 to the contract as the first constructor parameter
-  deployer.deploy(Betfunding);
-};
